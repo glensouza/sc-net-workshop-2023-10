bbecab5143bcbe7c182fac17e1b927289c76e054
Glen Souza <glenster75@gmail.com>
2023-10-16 14:49:14 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿/*
-this application will display option for user to select rock, paper or scissor
+/*
+this application will display option for user to select rock, paper, scissor, lizard or spock
 then the computer will randomly select an option
 then the application will display the result of the game
 */
 
 // display the welcome message
-Console.WriteLine("Welcome to Rock, Paper, Scissor Game!");
+Console.WriteLine("Welcome to Rock, Paper, Scissor, Lizard, Spock Game!");
 int userChoice = 0;
 int computerChoice = 0;
 
@@ -16,7 +16,9 @@ do
     Console.WriteLine("1. Rock");
     Console.WriteLine("2. Paper");
     Console.WriteLine("3. Scissor");
-    Console.WriteLine("4. Exit game");
+    Console.WriteLine("4. Lizard");
+    Console.WriteLine("5. Spock");
+    Console.WriteLine("6. Exit game");
 
     // get the user input
     Console.Write("Enter your choice: ");
@@ -26,7 +28,7 @@ do
         userInput = Console.ReadLine();
 
         // validate the user input
-        if (string.IsNullOrEmpty(userInput) || (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4"))
+        if (string.IsNullOrEmpty(userInput) || (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4" && userInput != "5" && userInput != "6"))
         {
             Console.WriteLine("Invalid input. Please try again.");
             Console.Write("Enter your choice: ");
@@ -37,11 +39,11 @@ do
     // convert the user input to integer
     userChoice = Convert.ToInt32(userInput);
 
-    if (userChoice != 4)
+    if (userChoice != 6)
     {
-        // generate a random number between 1 and 3
+        // generate a random number between 1 and 5
         Random random = new Random();
-        computerChoice = random.Next(1, 4);
+        computerChoice = random.Next(1, 6);
 
         switch (userChoice)
         {
@@ -53,6 +55,12 @@ do
                 break;
             case 3:
                 Console.WriteLine("You selected Scissor.");
+                break;
+            case 4:
+                Console.WriteLine("You selected Lizard.");
+                break;
+            case 5:
+                Console.WriteLine("You selected Spock.");
                 break;
         }
 
@@ -67,6 +75,12 @@ do
             case 3:
                 Console.WriteLine("Computer selected Scissor.");
                 break;
+            case 4:
+                Console.WriteLine("Computer selected Lizard.");
+                break;
+            case 5:
+                Console.WriteLine("Computer selected Spock.");
+                break;
         }
 
         // check the result
@@ -74,15 +88,23 @@ do
         {
             Console.WriteLine("It's a tie.");
         }
-        else if (userChoice == 1 && computerChoice == 3)
+        else if (userChoice == 1 && (computerChoice == 3 || computerChoice == 4))
         {
             Console.WriteLine("You won!");
         }
-        else if (userChoice == 2 && computerChoice == 1)
+        else if (userChoice == 2 && (computerChoice == 1 || computerChoice == 5))
         {
             Console.WriteLine("You won!");
         }
-        else if (userChoice == 3 && computerChoice == 2)
+        else if (userChoice == 3 && (computerChoice == 2 || computerChoice == 4))
+        {
+            Console.WriteLine("You won!");
+        }
+        else if (userChoice == 4 && (computerChoice == 2 || computerChoice == 5))
+        {
+            Console.WriteLine("You won!");
+        }
+        else if (userChoice == 5 && (computerChoice == 1 || computerChoice == 3))
         {
             Console.WriteLine("You won!");
         }
@@ -95,4 +117,4 @@ do
         Console.ReadLine();
         Console.Clear();
     }
-} while (userChoice != 4);
+} while (userChoice != 6);
